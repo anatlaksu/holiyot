9bcf6b0def3d447f3812ca2b3a56300c1214ba52
Antony Person <tony.personn@gmail.com>
2023-10-31 14:40:51 +0200

    Update  tables, and edit user with gof mangment

---
+++ backend/frontend/src/data/ConstCollectons/unitRequiredList.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable spaced-comment */
+/* eslint-disable no-unused-vars */
+
+const unitRequiredList = [
+  { id: "653fd730439371200e36ed67", name: 'מש"א' },
+  { id: "2", name: 'אגד טנ"א ארצי' },
+  { id: "3", name: 'חט"ל' },
+  { id: "4", name: 'רפ"ט' },
+  { id: "5", name: "תעשייה" },
+];
+
+export default unitRequiredList;
